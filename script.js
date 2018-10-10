8767cb6e16a8b0a35d4ccc8309dfdb757f3bd3ee
ShaneMarusczak <marusczaks@gmail.com>
2018-10-10 11:51:49 -0400

    added JS fade in

---
+++ script.js
@@ -0,0 +1,22 @@
+(function() {
+
+    // Select all elements directly within the body
+    const fadeElements = document.querySelectorAll('#wrapper > *');
+    console.log({ fadeElements });
+  
+    window.onscroll = function() {
+      console.log('scrolling...');
+  
+      for (let elem of fadeElements) {
+        if (elem.offsetTop + elem.offsetHeight < window.scrollY + 175) {
+          console.log('hide', elem);
+          elem.classList.add('fadeOut');
+        }
+        else {
+          console.log('show', elem);
+          elem.classList.remove('fadeOut');
+        }
+      }
+    };
+  
+  })();
