577c09af2e168d1d744f776ffc830c32592009b3
ShaneMarusczak <marusczaks@gmail.com>
2018-10-20 09:32:49 -0400

    made iife

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-(function() {
+(function () {
 
-    // Select all elements directly within the body
-    const fadeElements = document.querySelectorAll('#wrapper > *');
-    console.log({ fadeElements });
-  
-    window.onscroll = function() {
-      console.log('scrolling...');
-  
-      for (let elem of fadeElements) {
-        if (elem.offsetTop + elem.offsetHeight < window.scrollY + 150) {
-          console.log('hide', elem);
-          elem.classList.add('fadeOut');
-        }
-        else {
-          console.log('show', elem);
-          elem.classList.remove('fadeOut');
-        }
+  // Select all elements directly within the body
+  const fadeElements = document.querySelectorAll('#wrapper > *');
+  console.log({ fadeElements });
+
+  window.onscroll = function () {
+    console.log('scrolling...');
+
+    for (let elem of fadeElements) {
+      if (elem.offsetTop + elem.offsetHeight < window.scrollY + 150) {
+        console.log('hide', elem);
+        elem.classList.add('fadeOut');
       }
-    };
-  
-  })();
+      else {
+        console.log('show', elem);
+        elem.classList.remove('fadeOut');
+      }
+    }
+  };
 
+})();
+
+
+(function () {
   $(document).ready(function () {
     $("a").on('click', function (event) {
 
@@ -38,3 +40,4 @@
       }
     });
   });
+})();
