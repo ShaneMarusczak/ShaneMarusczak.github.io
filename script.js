7fb758733712c323ddcd83a81299f158c22fd54b
ShaneMarusczak <marusczaks@gmail.com>
2018-10-21 19:25:58 -0400

    added in modal box

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 (function () {
 
-  // Select all elements directly within the body
+  var modal = document.getElementById('myModal');
+
+var btn = document.getElementById("myBtn");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
   const fadeElements = document.querySelectorAll('#wrapper > *');
   console.log({ fadeElements });
 
