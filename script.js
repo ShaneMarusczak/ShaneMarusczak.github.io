6602f2ce63ef7b0bb67df8cd3f16117d04b97fac
ShaneMarusczak <marusczaks@gmail.com>
2019-01-13 14:09:28 -0500

    eliminated console logs

--- script.js
+++ script.js
@@ -20,15 +20,12 @@
   console.log({ fadeElements });
 
   window.onscroll = function () {
-    console.log('scrolling...');
 
     for (let elem of fadeElements) {
       if (elem.offsetTop + elem.offsetHeight < window.scrollY + 150) {
-        console.log('hide', elem);
         elem.classList.add('fadeOut');
       }
       else {
-        console.log('show', elem);
         elem.classList.remove('fadeOut');
       }
     }
